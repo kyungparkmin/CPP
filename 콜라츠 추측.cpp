371fd18139f6e3ed25e26dde6df80cf8cb1e4e3c
박경민 <87232158+kyungparkmin@users.noreply.github.com>
2022-02-01 15:25:18 +0900

    Update 콜라츠 추측.cpp

--- 콜라츠 추측.cpp
+++ 콜라츠 추측.cpp
@@ -2,40 +2,29 @@
 using namespace std;
 long long num=0;
 int cnt=0;
-int odd(){
-	num = (num * 3) + 1;
-	return num;
-}
-
-int even(){
-	num = num / 2;
-	return num;
-}
-
 int main(){
 	
 	cin>>num;
 	
 	while(true){
-		if(num%2==0){
-			even();
-			cnt++;
-		}else if(num%2==1){
-			odd();
-			cnt++;
-		}
+		
 		if(num==1){
 			cout<<cnt;
 			break;
 		}
+		
+		if(num%2==0){
+			num = num / 2;
+			cnt++;
+		}else{
+			num = (num * 3) + 1;
+			cnt++;
+		}
+		
 		if(cnt>=500){
 			cout<<"-1";
 			break;
 		}
 	}
-		
-	
-	
 }
-
 
